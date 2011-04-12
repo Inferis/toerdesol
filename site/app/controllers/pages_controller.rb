c4655aca14befc08cd57f1f9f2a7d6ac90a0cd2b
Inferis <spam@inferis.org>
2011-04-13 01:17:41 +0200

    First checkin.

---
+++ site/app/controllers/pages_controller.rb
@@ -0,0 +1,30 @@
+class PagesController < ApplicationController
+  def index 
+    render 'page'
+  end
+  
+  def koers
+    render 'page'
+  end
+
+  def parcours
+    render 'page'
+  end
+
+  def regels
+    render 'page'
+  end
+  
+  def geschiedenis
+    render 'page'
+  end
+
+  def fotos
+    render 'page'
+  end
+
+  def vrienden
+    render 'page'
+  end
+
+end
---
+++ site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '5db3e734b88f57cbddf8cb8ab9eb77e1e2dffeec4763089b397774df7452f90b648712a78519ee680a1162041aeccef5dd0d56c91a8a75619b24cfcdc6df9567'
